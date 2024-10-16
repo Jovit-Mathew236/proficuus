36266f43c517acd3fa6bc9ffc6d7b2e3acaf7120
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-10-16 09:34:52 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -8,12 +8,23 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0",
+          },
+          {
+            key: "Vercel-CDN-Cache-Control",
+            value: "no-store, max-age=0",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
-
-export const rewrites = {
-  async headers() {
-    return [{ source: "/(.*)", destination: "/" }];
-  },
-};
