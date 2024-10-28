d39065bb0d1e48ecae54b01b8e5e684643c82eea
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-10-29 01:50:01 +0530

    fix

--- app/api/registration/proficuus24/dashboard/route.ts
+++ app/api/registration/proficuus24/dashboard/route.ts
@@ -1,35 +1,21 @@
 export const fetchCache = "force-no-store";
 export const revalidate = 0; // To disable ISR.
-import { NextRequest, NextResponse } from "next/server";
-import { doc, getDoc } from "firebase/firestore";
+import { NextResponse } from "next/server";
+import { collection, getDocs, query, limit } from "firebase/firestore";
 import { db } from "@/lib/firebase/config";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
-    // Extract the UID from the query parameters
-    const { searchParams } = new URL(req.url);
-    const uid = searchParams.get("uid");
+    // Create a query with a limit higher than your expected document count
+    const q = query(collection(db, "volunteers"), limit(100));
 
-    // Validate UID presence
-    if (!uid) {
-      return NextResponse.json({ message: "UID is required" }, { status: 400 });
-    }
+    const querySnapshot = await getDocs(q);
+    const volunteers = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
 
-    // Get the document by UID
-    const userDocRef = doc(db, "volunteers", uid);
-    const userDoc = await getDoc(userDocRef);
-
-    // Check if the document exists
-    if (!userDoc.exists()) {
-      return NextResponse.json({ message: "User not found" }, { status: 404 });
-    }
-
-    const userData = {
-      id: userDoc.id,
-      ...userDoc.data(),
-    };
-
-    const response = NextResponse.json(userData, { status: 200 });
+    const response = NextResponse.json(volunteers, { status: 200 });
 
     // Disable caching by setting cache control headers
     response.headers.set(
@@ -46,9 +32,9 @@ export async function GET(req: NextRequest) {
 
     return response;
   } catch (error) {
-    console.error("Error fetching user details", error);
+    console.error("Error fetching volunteers", error);
     return NextResponse.json(
-      { message: "Error fetching user details" },
+      { message: "Error fetching volunteers" },
       { status: 500 }
     );
   }
