e9c8d01df5aeeccec337b5429d7cbdcf94c99860
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-10-31 05:57:27 +0530

    update config remove headers

--- next.config.mjs
+++ next.config.mjs
@@ -10,23 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, max-age=0",
-          },
-          {
-            key: "Vercel-CDN-Cache-Control",
-            value: "no-store, max-age=0",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
