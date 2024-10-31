ce23679a787cf464307302c1a28a5a198fe57ecf
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-10-31 16:56:53 +0530

    fix

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "@/lib/firebase/config";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export async function GET(request: NextRequest) {
