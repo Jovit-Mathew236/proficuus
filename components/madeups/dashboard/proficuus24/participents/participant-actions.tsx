6afaadc76c48dae9c60a58db3cf2871fd4dd5db0
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-12-04 12:03:05 +0530

    fix

--- components/madeups/dashboard/proficuus24/participents/participant-actions.tsx
+++ components/madeups/dashboard/proficuus24/participents/participant-actions.tsx
@@ -17,8 +17,12 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { DotsHorizontalIcon } from "@radix-ui/react-icons";
-import { useForm, FormProvider } from "react-hook-form"; // Import FormProvider
+import {
+  CaretSortIcon,
+  CheckIcon,
+  DotsHorizontalIcon,
+} from "@radix-ui/react-icons";
+import { useForm, FormProvider } from "react-hook-form";
 import { z } from "zod";
 import {
   InputOTP,
@@ -35,7 +39,22 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { Participant } from "./participants";
+import { collage, zone } from "@/lib/constants";
+import { cn } from "@/lib/utils";
 
 export const FormSchema = z.object({
   pin: z
@@ -43,10 +62,19 @@ export const FormSchema = z.object({
     .min(6, { message: "Your one-time password must be 6 characters." }),
 });
 
+export const EditFormSchema = z.object({
+  zone: z.string({
+    required_error: "Please select a Zone.",
+  }),
+  collage: z.string({
+    required_error: "College is required.",
+  }),
+});
+
 type Props = {
   participant: Participant;
   onSubmit: (
-    data: z.infer<typeof FormSchema>,
+    data: z.infer<typeof FormSchema> | z.infer<typeof EditFormSchema>,
     participant: Participant
   ) => void;
 };
@@ -57,38 +85,50 @@ const ParticipantActions = ({ participant, onSubmit }: Props) => {
     defaultValues: { pin: "" },
   });
 
+  const editForm = useForm({
+    resolver: zodResolver(EditFormSchema),
+    defaultValues: { zone: "", collage: "" },
+  });
+
   const [isDeleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
+  const [editFormOpen, setEditFormOpen] = useState(false);
+  const [collageOpen, setCollageOpen] = useState(false);
+  const [zoneOpen, setZoneOpen] = useState(false);
+  // const [isOtherSelected, setIsOtherSelected] = useState(false);
+  // const [otherCollage, setOtherCollage] = useState("");
 
   const handleSubmit = async (data: z.infer<typeof FormSchema>) => {
     await onSubmit(data, participant);
   };
 
+  const handleEditFormSubmit = async (data: z.infer<typeof EditFormSchema>) => {
+    await onSubmit(data, participant);
+  };
+
   const handleDelete = async () => {
-    // Close the confirmation dialog
     setDeleteConfirmationOpen(false);
 
-    try {
-      const response = await fetch(
-        `/api/dashboard/proficuus24/participants/delete`,
-        {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ id: participant.uid }), // Send participant ID for deletion
-        }
-      );
+    // try {
+    //   const response = await fetch(
+    //     `/api/dashboard/proficuus24/participants/delete`,
+    //     {
+    //       method: "DELETE",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify({ id: participant.uid }),
+    //     }
+    //   );
 
-      if (response.ok) {
-        alert("Participant deleted successfully");
-        // You can add logic to remove the participant from the UI list if needed
-      } else {
-        alert("Failed to delete participant");
-      }
-    } catch (error) {
-      console.error("Error deleting participant:", error);
-      alert("Error deleting participant");
-    }
+    //   if (response.ok) {
+    //     alert("Participant deleted successfully");
+    //   } else {
+    //     alert("Failed to delete participant");
+    //   }
+    // } catch (error) {
+    //   console.error("Error deleting participant:", error);
+    //   alert("Error deleting participant");
+    // }
   };
 
   return (
@@ -122,14 +162,15 @@ const ParticipantActions = ({ participant, onSubmit }: Props) => {
                 : "Mark as Coordinator"}
             </DropdownMenuItem>
           </DialogTrigger>
-          {/* Trigger confirmation dialog on delete click */}
+          <DropdownMenuItem onClick={() => setEditFormOpen(true)}>
+            Edit
+          </DropdownMenuItem>
           <DropdownMenuItem onClick={() => setDeleteConfirmationOpen(true)}>
             Delete
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
 
-      {/* Delete Confirmation Dialog */}
       <Dialog
         open={isDeleteConfirmationOpen}
         onOpenChange={setDeleteConfirmationOpen}
@@ -150,6 +191,151 @@ const ParticipantActions = ({ participant, onSubmit }: Props) => {
           >
             Cancel
           </Button>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog open={editFormOpen} onOpenChange={setEditFormOpen}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit the collage and zone</DialogTitle>
+            <DialogDescription>
+              This action will edit the collage and zone of the participant.
+            </DialogDescription>
+          </DialogHeader>
+          <FormProvider {...editForm}>
+            <form
+              onSubmit={editForm.handleSubmit(handleEditFormSubmit)}
+              className="space-y-8 w-full"
+            >
+              <FormField
+                control={editForm.control}
+                name="zone"
+                render={({ field }) => (
+                  <FormItem className="flex w-1/2 flex-col">
+                    <FormLabel>Zone *</FormLabel>
+                    <Popover open={zoneOpen} onOpenChange={setZoneOpen}>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant="outline"
+                            role="combobox"
+                            className={cn(
+                              "w-full justify-between",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value
+                              ? zone.find((zone) => zone.value === field.value)
+                                  ?.label
+                              : "Select Zone"}
+                            <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-full p-0">
+                        <Command>
+                          <CommandInput placeholder="Search Zone..." />
+                          <CommandList>
+                            <CommandEmpty>No Zone found.</CommandEmpty>
+                            <CommandGroup>
+                              {zone.map((zone) => (
+                                <CommandItem
+                                  value={zone.label}
+                                  key={zone.value}
+                                  onSelect={() => {
+                                    editForm.setValue("zone", zone.value);
+                                    setZoneOpen(false);
+                                  }}
+                                >
+                                  <CheckIcon
+                                    className={cn(
+                                      "mr-2 h-4 w-4",
+                                      zone.value === field.value &&
+                                        "opacity-100"
+                                    )}
+                                  />
+                                  {zone.label}
+                                </CommandItem>
+                              ))}
+                            </CommandGroup>
+                          </CommandList>
+                        </Command>
+                      </PopoverContent>
+                    </Popover>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Collage Selection */}
+              <FormField
+                control={editForm.control}
+                name="collage"
+                render={({ field }) => (
+                  <FormItem className="flex w-1/2 flex-col">
+                    <FormLabel>Collage *</FormLabel>
+                    <Popover open={collageOpen} onOpenChange={setCollageOpen}>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant="outline"
+                            role="combobox"
+                            className={cn(
+                              "w-full justify-between",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value
+                              ? collage.find(
+                                  (collage) => collage.value === field.value
+                                )?.label
+                              : "Select Collage"}
+                            <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-full p-0">
+                        <Command>
+                          <CommandInput placeholder="Search Collage..." />
+                          <CommandList>
+                            <CommandEmpty>No Collage found.</CommandEmpty>
+                            <CommandGroup>
+                              {collage.map((collage) => (
+                                <CommandItem
+                                  value={collage.label}
+                                  key={collage.value}
+                                  onSelect={() => {
+                                    editForm.setValue("collage", collage.value);
+                                    setCollageOpen(false);
+                                  }}
+                                >
+                                  <CheckIcon
+                                    className={cn(
+                                      "mr-2 h-4 w-4",
+                                      collage.value === field.value &&
+                                        "opacity-100"
+                                    )}
+                                  />
+                                  {collage.label}
+                                </CommandItem>
+                              ))}
+                            </CommandGroup>
+                          </CommandList>
+                        </Command>
+                      </PopoverContent>
+                    </Popover>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" variant="default">
+                Submit
+              </Button>
+              <Button variant="outline" onClick={() => setEditFormOpen(false)}>
+                Cancel
+              </Button>
+            </form>
+          </FormProvider>
         </DialogContent>
       </Dialog>
 
